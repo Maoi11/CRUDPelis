9362dc16149eff2743f408ff450afc939260ae8c
Maoi11 <marisaindianagarat@gmail.com>
2023-04-29 22:52:51 -0300

    intento traer elemento iconTheme

---
+++ js/app.js
@@ -0,0 +1,19 @@
+//leer localStorage
+let temaConfigurado = JSON.parse(localStorage.getItem('tema')) || 'dark';
+
+cambiarTema(temaConfigurado);
+
+let btnThemeDark = document.querySelector('#btn-themeDark');
+let btnThemeLight = document.querySelector('#btn-themeLight');
+
+let iconTheme = document.getElementById('iconTheme');
+console.log(iconTheme);
+
+btnThemeDark.addEventListener('click', () => cambiarTema('dark'));
+btnThemeLight.addEventListener('click', () => cambiarTema('light'));
+
+function cambiarTema(color) {
+    document.querySelector('body').setAttribute('data-bs-theme', color);
+    localStorage.setItem('tema', JSON.stringify(color));
+    
+}
