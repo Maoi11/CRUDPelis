c37b266ba0f555b05f11924e43320963ec6f2d2f
Maoi11 <marisaindianagarat@gmail.com>
2023-05-01 19:16:19 -0300

    agregamos expresion regular para validar duracion

--- js/admin.js
+++ js/admin.js
@@ -3,19 +3,16 @@ import { sumarioValidacion } from "./helpers.js";
 
 let formAdminPelicula = document.getElementById("formPelicula");
 let listaDePeliculas = [];
-let codigo = document.getElementById('codigo'),
-titulo = document.getElementById('titulo'),
-descripcion = document.getElementById('descripcion'),
-imagen = document.getElementById('imagen'),
-genero = document.getElementById('genero'),
-anio = document.getElementById('anio'),
-duracion = document.getElementById('duracion'),
-director = document.getElementById('director'),
-reparto = document.getElementById('reparto'),
-pais = document.getElementById('pais');
-
-
-
+let codigo = document.getElementById("codigo"),
+  titulo = document.getElementById("titulo"),
+  descripcion = document.getElementById("descripcion"),
+  imagen = document.getElementById("imagen"),
+  genero = document.getElementById("genero"),
+  anio = document.getElementById("anio"),
+  duracion = document.getElementById("duracion"),
+  director = document.getElementById("director"),
+  reparto = document.getElementById("reparto"),
+  pais = document.getElementById("pais");
 
 formAdminPelicula.addEventListener("submit", prepararFormulario);
 
@@ -27,35 +24,32 @@ function prepararFormulario(e) {
 
 function crearPelicula() {
   // validar datos del formulario
-  let resumen = sumarioValidacion("algun texto!!");
+  let resumen = sumarioValidacion(titulo.value, descripcion.value, duracion.value);
   if (resumen.length === 0) {
     // los datos son validos
-     //crear pelicula
-  const peliculaNueva = new Pelicula(
-    "Título de la película",
-    "Descripción de la película",
-    "imagen.jpg",
-    "Drama",
-    2023,
-    120,
-    "Estados Unidos",
-    "Director de la película",
-    ["Actor 1", "Actor 2", "Actor 3"]
-  );
-  console.log(peliculaNueva);
-  // lo agrego en un array
-  listaDePeliculas.push(peliculaNueva);
-  console.log(listaDePeliculas);
-  // almaceno el array en localStorage
-  localStorage.setItem("listaDePeliculas", JSON.stringify(listaDePeliculas));
-  // cierro el modal del form
+    //crear pelicula
+    const peliculaNueva = new Pelicula(
+      titulo.value,
+      descripcion.value,
+      imagen.value,
+      genero.value,
+      anio.value,
+      duracion.value,
+      pais.value,
+      director.value,
+      reparto.value
+    );
+    console.log(peliculaNueva);
+    // lo agrego en un array
+    listaDePeliculas.push(peliculaNueva);
+    console.log(listaDePeliculas);
+    // almaceno el array en localStorage
+    localStorage.setItem("listaDePeliculas", JSON.stringify(listaDePeliculas));
+    // cierro el modal del form
   } else {
     // los datos no son validos -> mostrar alert
-    let alertError = document.getElementById('alerta');
+    let alertError = document.getElementById("alerta");
     alertError.innerHTML = resumen;
-    alertError.className = 'alert alert-danger mt-3'
-
+    alertError.className = "alert alert-danger mt-3";
   }
-
- 
 }
--- js/helpers.js
+++ js/helpers.js
@@ -1,5 +1,5 @@
 function validarCantidadCaracteres(texto, min, max) {
-  if (texto.length >= min && texto <= max) {
+  if (texto.length >= min && texto.length <= max) {
     console.log("dato correcto");
     return true;
   } else {
@@ -8,10 +8,28 @@ function validarCantidadCaracteres(texto, min, max) {
   }
 }
 
-export function sumarioValidacion(titulo){
+function validarDuracion (tiempo) {
+    let regEx = /^\d{1,3}$/;
+    if(regEx.test(tiempo)){
+        console.log('la expresion regular funciona');
+        return true
+    } else {
+        console.log('la expresion regular falló');
+        return false
+    }
+
+}
+
+export function sumarioValidacion(titulo, descripcion, duracion){
     let resumen = '';
     if(!validarCantidadCaracteres(titulo,2,100)){
-resumen += 'El título ingresado no es válido. Debe tener entre 2 y 100 caracteres' 
+resumen += 'El título ingresado no es válido. Debe tener entre 2 y 100 caracteres. <br>' 
+    }
+    if(!validarCantidadCaracteres(descripcion,5,500)){
+        resumen += 'La descripción de la película debe tener entre 5 y 500 caracteres. <br>' 
+            }
+    if (!validarDuracion(duracion)){
+        resumen += 'La duración debe ser una cantidad de minutos de entre 1 y 3 caracteres numericos. <br>'
     }
     return resumen;
 }
